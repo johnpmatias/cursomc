79448eebe3af7bad964ec8ea34b5a5e9cba52efa
John Matias <johnp.matias@gmail.com>
2018-06-14 19:48:02 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/johnmatias/cursomc/domain/Cliente.java
+++ src/main/java/com/johnmatias/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -128,6 +131,14 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	
