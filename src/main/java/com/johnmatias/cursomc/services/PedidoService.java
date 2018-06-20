71bdb04ead619677d811d4ac0f481be193f913f0
John Matias <johnp.matias@gmail.com>
2018-06-20 19:23:51 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/johnmatias/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.johnmatias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.johnmatias.cursomc.domain.Pedido;
+import com.johnmatias.cursomc.repositories.PedidoRepository;
+import com.johnmatias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" + id +", Tipo:" + Pedido.class.getName()));
+	}
+	
+}
